8df24ad931e4ef0415c7eda7a7f25f5e39f82cf4
DimmyMaenhout <dimmy.maenhout@telenet.be>
2018-10-06 16:50:01 +0200

    removed director & writer

--- DiMovies2/Model/Movie.swift
+++ DiMovies2/Model/Movie.swift
@@ -24,15 +24,13 @@ class Movie {
     var video = false
     var vote_average : Double?
     var vote_count : Int?
-    var director = ""
-    var writer = ""
     var stars = ""
     var genres : [String] = []            //film heeft een array waarin id's (int) en name (string) zit dus gaan we name ophalen
     //    var poster : UIImage = nil
     var trailerUrl = ""
     
     
-    convenience init(movie_id: Int, imdb_id: String, title: String, overview: String, duration: Int, budget: Double, popularity: Double, releaseDate: String, revenue: Double, status: String, tagline: String, video: Bool, vote_average: Double, votecount: Int, writer : String, director :  String, stars: String, genres : [String], poster_path: String, trailerUrl: String) {
+    convenience init(movie_id: Int, imdb_id: String, title: String, overview: String, duration: Int, budget: Double, popularity: Double, releaseDate: String, revenue: Double, status: String, tagline: String, video: Bool, vote_average: Double, votecount: Int, stars: String, genres : [String], poster_path: String, trailerUrl: String) {
         
         self.init()
         self.id = movie_id
@@ -49,12 +47,13 @@ class Movie {
         self.video = video
         self.vote_average = vote_average
         self.vote_count = votecount
-        self.director = director
-        self.writer = writer
         self.stars = stars
         self.genres = genres
         self.poster_path = poster_path
         self.trailerUrl = trailerUrl
     }
-    
+}
+
+enum Type: Int {
+    case Movies = 0, Series
 }
