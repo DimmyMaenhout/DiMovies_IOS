4ef3b7e0262de8bd96cd73b876817ff4f7f6bdfa
Dimmy Maenhout <DimmyMaenhout@macbook-pro-van-dimmy.local>
2018-11-03 16:22:55 +0100

    Added 2 default collections

--- DiMovies2/Repo/DataRepo.swift
+++ DiMovies2/Repo/DataRepo.swift
@@ -12,10 +12,16 @@ class DataRepo {
     
     init() {
         let user = User(username: "User")
+        
+        let moviesSeen = Collection(name: "Seen", id: 0)
+        let wantToWatchMovies = Collection(name: "Want to watch", id: 1)
         let realm = try! Realm()
         
         try! realm.write {
             realm.add(user)
+            
+            user.collections.append(moviesSeen)
+            user.collections.append(wantToWatchMovies)
         }
     }
 }
