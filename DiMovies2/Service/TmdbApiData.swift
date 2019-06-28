a3beeb3f675708eb0330a99287b8fde211620402
DimmyMaenhout <dimmy.maenhout@telenet.be>
2019-06-28 09:21:29 +0200

    Added movies playing url

--- DiMovies2/Service/TmdbApiData.swift
+++ DiMovies2/Service/TmdbApiData.swift
@@ -3,6 +3,7 @@ import Foundation
 struct TmdbApiData {
 
     static let baseURL_TMDB = "https://api.themoviedb.org/3"
+    static let moviesPlayingURL = "/movie/now_playing?"
     //voor poster
     static let baseUrlPoster = "https://image.tmdb.org/t/p/"
     static let originalSizePoster = "original"
